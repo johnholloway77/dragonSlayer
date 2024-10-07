6013c0a39261c695977a4253caf849647ebe239c
johnholloway77 <john.holloway@edu.sait.ca>
2024-10-06 22:42:39 -0600

    Creating rooms

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "include/Player.h"
 #include "include/Sword.h"
+#include "include/Room.h"
 
 int main() {
     std::cout << "Hello, what is your name" << std::endl;
@@ -8,27 +9,37 @@ int main() {
 
     std::cin >> name;
 
-    Player p1 = Player(name, 100);
-    while(p1.isAlive()){
-        Sword s1 = Sword("StormBringer");
-        Sword s2 = Sword("DeathBringer", "A deadly sword made cursed with the soul of its enemies", 15);
+    Player *p1 = new Player(name, 100);
+    Sword *s1 = new Sword("StormBringer");
+    Sword *s2 = new Sword("DeathBringer", "A deadly sword made cursed with the soul of its enemies", 15);
 
-        std::cout << p1.getName() << std::endl;
-        std::cout << p1.listInventory() << std::endl;
+    while(p1->isAlive()){
 
-        std::cout << "health: " << p1.getHealth() << std::endl;
-        std::cout << "is " << p1.getName() << " alive? " << (p1.isAlive() ? "Yes!" : "No") << std::endl;
+        std::cout << p1->getName() << std::endl;
+        std::cout << p1->listInventory() << std::endl;
 
-        p1.addToInventory(&s1);
-        p1.addToInventory(&s2);
-        std::cout << p1.listInventory() << std::endl;
+        std::cout << "health: " << p1->getHealth() << std::endl;
+        std::cout << "is " << p1->getName() << " alive? " << (p1->isAlive() ? "Yes!" : "No") << std::endl;
 
-        p1.hurt(100);
-        std::cout << "health: " << p1.getHealth() << std::endl;
-        std::cout << "is " << p1.getName() << " alive? " << (p1.isAlive() ? "Yes!" : "No") << std::endl;
+        p1->addToInventory(s1);
+        p1->addToInventory(s2);
+        std::cout << p1->listInventory() << std::endl;
+
+        p1->hurt(100);
+        std::cout << "health: " << p1->getHealth() << std::endl;
+        std::cout << "is " << p1->getName() << " alive? " << (p1->isAlive() ? "Yes!" : "No") << std::endl;
     }
 
-    std::cout << "You're dead " << p1.getName() << "!" << std::endl;
+    std::cout << "You're dead " << p1->getName() << "!" << std::endl;
+
+    Room *field = new Room("field", "A vast field");
+
+    std::cout << field->getName() << " " << field->getDescription() << std::endl;
+    std::cout << "N: " << field->lookNorth() << ". E " << field->lookEast() << ", S " << field->lookSouth() << ", W: " << field->lookWest() << std::endl;
+
+    delete p1;
+    delete s1;
+    delete s2;
 
     return 0;
 }
