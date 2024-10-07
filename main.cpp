dea158847bd28e655d6d0c940ad6c02ef86ab132
johnholloway77 <john.holloway@edu.sait.ca>
2024-10-07 02:10:12 -0600

    ncurses

--- main.cpp
+++ main.cpp
@@ -1,35 +1,70 @@
 #include <iostream>
+#include <ncurses.h>
+#include <cctype>
 #include "include/Player.h"
 #include "include/Sword.h"
 #include "include/Room.h"
 #include "include/Food.h"
 
 int main() {
-    std::cout << "Hello, what is your name" << std::endl;
-    std::string name;
 
-    std::cin >> name;
+    std::cout << "\033[?1049h";
+    fflush(stdout);
 
-    Player *p1 = new Player(name, 100, "Our lowly hero");
-    Sword *s1 = new Sword("StormBringer");
-    Sword *s2 = new Sword("DeathBringer", "A deadly sword made cursed with the soul of its enemies", 15);
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
 
-    Food *muffin = new Food("muffin");
+    int screen_height;
+    int screen_width;
+    getmaxyx(stdscr, screen_height, screen_width);
+
+    int input_height = screen_height / 5;
+    int display_height = screen_height - input_height;
+
+    WINDOW* display_win = newwin(display_height, screen_width, 0, 0);
+    WINDOW* input_win = newwin(input_height, screen_width, display_height, 0);
+
+    scrollok(display_win, true);
+    box(display_win, 0, 0);
+    box(input_win, 0, 0);
+
+    wrefresh(display_win);
+    wrefresh(input_win);
+    int input_win_width = screen_width - 2; // Window width excluding borders
+    int display_win_width = screen_width - 2; // Window width excluding border
+
+    std::string input;
+    bool input_started = false;
+
+    while(1){
+
+        std::cout << "Hello, what is your name" << std::endl;
+        std::string name;
+
+        std::cin >> name;
+
+        Player *p1 = new Player(name, 100, "Our lowly hero");
+        Sword *s1 = new Sword("StormBringer");
+        Sword *s2 = new Sword("DeathBringer", "A deadly sword made cursed with the soul of its enemies", 15);
+
+        Food *muffin = new Food("muffin");
 
 
-    std::cout << p1->getName() << std::endl;
-    std::cout << p1->getDescription() << std::endl;
-    std::cout << p1->listInventory() << std::endl;
+        std::cout << p1->getName() << std::endl;
+        std::cout << p1->getDescription() << std::endl;
+        std::cout << p1->listInventory() << std::endl;
 
-    std::cout << "health: " << p1->getHealth() << std::endl;
-    std::cout << "is " << p1->getName() << " alive? " << (p1->isAlive() ? "Yes!" : "No") << std::endl;
+        std::cout << "health: " << p1->getHealth() << std::endl;
+        std::cout << "is " << p1->getName() << " alive? " << (p1->isAlive() ? "Yes!" : "No") << std::endl;
 
-    p1->addItem(s1);
-    p1->addItem(s2);
-    p1->addItem(muffin);
-    std::cout << p1->listInventory() << std::endl;
+        p1->addItem(s1);
+        p1->addItem(s2);
+        p1->addItem(muffin);
+        std::cout << p1->listInventory() << std::endl;
 
-    //while(p1->isAlive()){
+        //while(p1->isAlive()){
 
         std::cout << p1->attack() << std::endl;
 
@@ -48,27 +83,28 @@ int main() {
         std::cout << p1->useItem(muffin) << std::endl;
 
         std::cout << p1->listInventory() << std::endl;
-    //}
+        //}
 
 
 
-    //std::cout << "You're dead " << p1->getName() << "!" << std::endl;
+        //std::cout << "You're dead " << p1->getName() << "!" << std::endl;
 
-    Room *field = new Room("field", "A vast field");
+        Room *field = new Room("field", "A vast field");
 
-    std::cout << field->getName() << " " << field->getDescription() << std::endl;
-    std::cout << "N: " << field->lookNorth() << ". E " << field->lookEast() << ", S " << field->lookSouth() << ", W: " << field->lookWest() << std::endl;
+        std::cout << field->getName() << " " << field->getDescription() << std::endl;
+        std::cout << "N: " << field->lookNorth() << ". E " << field->lookEast() << ", S " << field->lookSouth() << ", W: " << field->lookWest() << std::endl;
 
-    std::cout << "The following items are in " << field->getName() << "\n" << field->listItems() << std::endl;
-    field->addItem(s1);
-    field->addItem(s2);
-    std::cout << "The following items are in " << field->getName() << "\n" << field->listItems() << std::endl;
+        std::cout << "The following items are in " << field->getName() << "\n" << field->listItems() << std::endl;
+        field->addItem(s1);
+        field->addItem(s2);
+        std::cout << "The following items are in " << field->getName() << "\n" << field->listItems() << std::endl;
 
 
-    delete p1;
-    delete s1;
-    delete s2;
-    delete field;
+        delete p1;
+        delete s1;
+        delete s2;
+        delete field;
+    }
 
     return 0;
 }
