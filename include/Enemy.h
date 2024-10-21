08e738ad216ffd08cdebdaa5103cfa8ec80e66c3
johnholloway77 <john.holloway@edu.sait.ca>
2024-10-20 23:33:07 -0600

    added additional descriptions to methods

--- include/Enemy.h
+++ include/Enemy.h
@@ -11,13 +11,35 @@
 #ifndef INCLUDE_ENEMY_H_
 #define INCLUDE_ENEMY_H_
 
+/**
+ * @class Enemy Enemy.h "Enemy.h"
+ * @brief A class for enemy objects. Class extends abstract class creature
+ */
 class Enemy : public Creature {
  public:
+  /**
+   * @brief The constructor to be used for creating an enemy
+   * @param name A string for the enemy's name
+   * @param health  The starting health for the enemy
+   * @param desc  A description of the enemy.
+   */
   Enemy(const std::string& name, int health, const std::string& desc)
       : Creature(name, health, desc) {}
 
+  /**
+   * @brief An identifier for what type of creature this is. Specifies enemy.
+   * @return a string stating "Enemy"
+   */
   std::string getType() const override { return "Enemy"; }
 
+  /**
+   * @brief A function used to attack an creature (usually the player). Unlike
+   * the other  attack functions, enemies can only attack with a Weapon object
+   * @param creature A pointer to the creature to be attacked
+   * @param weapon A pointer to the weapon being used for the attack
+   * @return A string describing the attack against creature and the damage
+   * inflected
+   */
   std::string attack(Creature* creature, Weapon* weapon);
 };
 
--- include/ExitRoom.h
+++ include/ExitRoom.h
@@ -8,9 +8,19 @@
 
 #include "Game.h"
 #include "Room.h"
+/**
+ * @class ExitRoom ExitRoom.h "ExitRoom.h"
+ * @brief A special type of room object that is designated as an exit room. Used
+ * to end the game and display a congrats page when loaded
+ */
 class ExitRoom : public Room {
  private:
  public:
+  /**
+   * @brief A constructor for the exit room
+   * @param name A const reference for the name of the exit room
+   * @param desc  A const reference for the description of the exit room
+   */
   ExitRoom(const std::string& name, const std::string& desc)
       : Room(name, desc) {}
 };
